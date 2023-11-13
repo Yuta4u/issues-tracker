d342e194b7c14e777a585b979b5c5cd9d4b34809
Yuta4u <yudhistira4u@gmail.com>
2023-11-13 12:44:07 +0700

    buat issue page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 
-const page = () => {
-  return <div>test</div>
+const IssuesPage = () => {
+  return <div>ini issue</div>
 }
 
-export default page
+export default IssuesPage
