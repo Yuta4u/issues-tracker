27179cd5b63fccd9ee6bdaa9b0ffbd84d790e82b
Yuta4u <yudhistira4u@gmail.com>
2023-11-15 13:02:24 +0700

    api for creating issue

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -18,6 +18,10 @@ const NavBar = () => {
       label: "Issues",
       href: "/issues",
     },
+    {
+      label: "Test",
+      href: "/test",
+    },
   ]
 
   return (
---
+++ app/test/page.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const TestPage = () => {
+  return <div>ini test</div>
+}
+
+export default TestPage
